7277b680ce66d2df5fcc14f8a1b12032a865bbb6
Nabil Benketaf <nbenketaf@gmail.com>
2015-05-27 23:53:01 +0200

    alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,22 @@
 (ns alphabet-cipher.coder)
 
+(defn idx [c]
+  (- (int c) 97)) ;; int \a = 97
+
+(defn char-at [i]
+  (char (+ i 97)))
+
+(defn encode-pair [k m]
+  (char (+ (mod (+ (idx k) (idx m)) 26) 97))) ;; 26 = nb of letters in the alphabet
+
+(defn decode-pair [k m]
+  (char (+ (mod (- (idx m) (idx k)) 26) 97)))
+
+(defn pairs [keyword message]
+  (map vector (cycle keyword) message))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map #(apply encode-pair %) (pairs keyword message))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map #(apply decode-pair %) (pairs keyword message))))
