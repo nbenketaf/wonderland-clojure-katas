f6f263dc8a0ee290ebc446c3001d3d8aafb4f1ba
Nabil Benketaf <nbenketaf@gmail.com>
2015-05-28 00:19:28 +0200

    refactoring

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,22 +1,27 @@
 (ns alphabet-cipher.coder)
 
+(def nb-chars 26)
+
 (defn idx [c]
-  (- (int c) 97)) ;; int \a = 97
+  (- (int c) (int \a))) ;; int \a = 97
 
 (defn char-at [i]
-  (char (+ i 97)))
+  (char (+ i (int \a))))
 
 (defn encode-pair [k m]
-  (char (+ (mod (+ (idx k) (idx m)) 26) 97))) ;; 26 = nb of letters in the alphabet
+  (char-at (mod (+ (idx k) (idx m)) nb-chars))) ;; 26 = nb of letters in the alphabet
 
 (defn decode-pair [k m]
-  (char (+ (mod (- (idx m) (idx k)) 26) 97)))
+  (char-at (mod (- (idx m) (idx k)) nb-chars)))
 
 (defn pairs [keyword message]
   (map vector (cycle keyword) message))
 
+(defn *code [keyword message f]
+  (apply str (map #(apply f %) (pairs keyword message))))
+
 (defn encode [keyword message]
-  (apply str (map #(apply encode-pair %) (pairs keyword message))))
+  (*code keyword message encode-pair))
 
 (defn decode [keyword message]
-  (apply str (map #(apply decode-pair %) (pairs keyword message))))
+  (*code keyword message decode-pair))
