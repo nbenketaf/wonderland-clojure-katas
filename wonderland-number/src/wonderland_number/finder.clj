11fe8f70a7a1eea895e3cd8efd51f4e03c89c523
Nabil Benketaf <nbenketaf@gmail.com>
2015-09-04 08:37:42 +0200

    wonderland number solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,12 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [n m]
+  (= (set (str n)) (set (str m))))
+
+(defn is-wonderland-number [n]
+  (every? #(same-digits? n (* n %)) (range 2 7)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range 100000 1000000)
+       (filter #(is-wonderland-number %))
+       first))
